45f9804a7e8c45be8034d2a3f8a6c2491c1c94ab
javimgit1224 <36682043+javimgit1224@users.noreply.github.com>
2018-03-15 13:14:53 -0700

    Update x86_2.asm
    added code to loop and receive inputted numbers until ctrl+d

--- x86_2.asm
+++ x86_2.asm
@@ -16,5 +16,51 @@ thanks db "thank you for your inputs. your array is as follows: ",10,0
 swapping db "the software is now swapping the largest and smallest numbers in the array. finished",10 "your array is now the following. ",10,0
 goodbye db "this assembly function will now return to the caller function.",10,0
 
+string_format db "%s", 0	;general string format
+float_format db "%lf",0		;general 8 byte format 
 
+segment .bss	;declare pointer to not initialized space in this segment
 
+;begin application
+segment .text	;execute instructions
+
+array:	;execution begins here
+
+;start of assembly program
+push rbp
+mov rbp, rsp
+
+;show initial message
+mov qword rax, 0
+mov rdi, string_format
+mov rsi, initial
+call printf wrt ..plt	;call print using pic technique 
+
+;prompt for float point numbers
+mov qword rax, 0
+mov rdi, prompt
+call printf wrt ..plt
+
+;loop to receive inputted numbers unitl control+d
+mov r15,0	;start counter at 0
+begin_loop:
+
+push qword 0
+mov qword rax, 0
+mov rdi, float_format
+mov rsi, rsp
+call scanf wrt ..plt
+cdq
+shl rdx, 32
+or rax, rdx
+cmp rax, -1
+je loop_finished
+movsd xmm0, [rsp]
+pop rax
+inc r15
+jmp begin_loop
+
+loop_finished:
+pop rax
+
+ 
